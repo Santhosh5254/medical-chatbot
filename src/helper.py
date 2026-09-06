119b1c66142860def7c9e9fa180e01aacef6d693
Santhosh Kumar <rakeshnelluri123@email.com>
2026-09-06 18:25:33 +0530

    Optimize embeddings for serverless deployment

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,6 @@
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import FastEmbedEmbeddings
 from typing import List
 from langchain.schema import Document
 
@@ -43,7 +43,9 @@ def text_split(extracted_data):
 
 
 
-#Download the Embeddings from HuggingFace 
+# Download embeddings using FastEmbed
 def download_hugging_face_embeddings():
-    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
+    embeddings = FastEmbedEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
     return embeddings
